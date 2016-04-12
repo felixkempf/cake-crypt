ec13855c3f3c35caae1a44da4fde0a0f112ad9b5
Felix Kempf <f.kempf@shifu.me>
2016-04-13 00:26:30 +0200

    enabled matching in finder, who now also always returns a Query object

--- src/Model/Table/Behavior/CryptBehavior.php
+++ src/Model/Table/Behavior/CryptBehavior.php
@@ -20,10 +20,18 @@ class CryptBehavior extends Behavior
     public function findEncrypted(Query $query, array $options)
     {
         if ($query->count() === 0) {
-            return [];
+            return $query;
         }
         $Model = $query->repository();
-        $all = $query->limit(1000)->all();
+        $containMatching = false;
+        if (!empty($options['matching']) ) {
+            $containMatching = array_keys($options['matching']);
+        }
+
+        $all = $query
+            ->contain($containMatching)
+            ->limit(1000)
+            ->all();
 
         if (!empty($options['where']) ) {
             foreach ($options['where'] as $fieldname => $fieldvalue) {
@@ -43,9 +51,34 @@ class CryptBehavior extends Behavior
                 }
             }
         }
+        unset($fieldname, $fieldvalue);
+
+        if (!empty($options['matching']) ) {
+            foreach ($options['matching'] as $modelName => $matchingConditions) {
+                $entitizedModelName = Inflector::underscore(Inflector::singularize($modelName));
+                $all = $all->filter(function($value, $key) use ($matchingConditions, $entitizedModelName) {
+                    // if associated entity is empty, accept result with a disagreeing shake of the head
+                    if (empty($value->$entitizedModelName)) {
+                        return true;
+                    }
+                    $pass = true;
+                    foreach ($matchingConditions as $fieldname => $fieldvalue) {
+                        // reject result only if field in associated entity is set
+                        // and does not fullfill the conditions
+                        if (isset($value->$entitizedModelName->$fieldname) && $value->$entitizedModelName->$fieldname != $fieldvalue) {
+                            $pass = false;
+                            break;
+                        }
+                    }
+                    return $pass;
+                });
+            }
+        }
+
         $ids = $all->extract('id')->toArray();
         if (count($ids) === 0) {
-            return [];
+            // needs to return Query, so we mock one which should not find anything
+            return $Model->find()->where(['id' => '']);
         }
         $contain = empty($options['contain_']) ? [] : $options['contain_'];
         $select = (empty($options['select'])) ? [] : $options['select'];
